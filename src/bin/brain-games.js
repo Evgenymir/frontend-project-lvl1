347fdeaf922e614e9d240ff4435f7573bf92d014
Evgeny <bespalove.mir@gmail.com>
2019-12-08 12:15:06 +0300

    Прошел 4 задание

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import {userName} from '../index.js';
+import userName from '../index';
 
 console.log('Welcome to the Brain Games!');
-console.log('Hi ' + userName + '!');
+console.log(`Hi ${userName} !`);
