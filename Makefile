7a02521bb6900ae84d5ba4df833accacd7152acf
Evgeny <bespalove.mir@gmail.com>
2019-12-15 15:39:36 +0300

    Реализовывает необходимый вывод игры

--- Makefile
+++ Makefile
@@ -12,3 +12,6 @@ lint:
 
 brain-even:
 	npm run babel-node src/bin/brain-even.js
+
+brain-calc:
+	npm run babel-node src/bin/brain-calc.js
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
 // Рандомное число
-export const randomNumber = () => Math.floor(Math.random() * 20) + 1;
+export const randomNumber = (min, max) => Math.floor(Math.random() * (max - min) + min);
 
 console.log('Welcome to the Brain Games!');
 
@@ -19,7 +19,7 @@ export const gameEven = () => {
     console.log('\n');
 
     const gameEvenBeginning = (counter = 0) => {
-        const questionNumber = randomNumber();
+        const questionNumber = randomNumber(1, 20);
         console.log(`Question: ${questionNumber}`);
 
         const userAnswer = readlineSync.question('Your answer: ');
@@ -39,4 +39,56 @@ export const gameEven = () => {
     };
 
     gameEvenBeginning(0);
+};
+
+// Игра Brain-calc
+
+export const gameCalc = () => {
+    console.log('What is the result of the expression?');
+    console.log('');
+    const name = getUserName();
+    console.log('');
+
+    const gameCalcBeginning = (counter = 0) => {
+        const numberOne = randomNumber(1, 20);
+        const numberTwo = randomNumber(1, 20);
+        const isOperandIndex = randomNumber(1, 4);
+        let operand = '';
+        let correctAnswer = '';
+
+        if (isOperandIndex === 1) {
+            operand = '+';
+            correctAnswer = numberOne + numberTwo;
+        }
+
+        if (isOperandIndex === 2) {
+            operand = '-';
+            correctAnswer = numberOne - numberTwo;
+        }
+
+        if (isOperandIndex === 3) {
+            operand = '*';
+            correctAnswer = numberOne * numberTwo;
+        }
+
+        console.log(`Question: ${numberOne} ${operand} ${numberTwo}`);
+
+        const userAnswer = readlineSync.question('Your answer: ');
+
+        console.log(typeof correctAnswer);
+        console.log(typeof userAnswer);
+
+        if (correctAnswer.toString() === userAnswer) {
+            if (counter === 2) {
+                return console.log(`Congratulations, ${name}!`);
+            }
+
+            console.log('Correct!');
+            return gameCalcBeginning(counter + 1);
+        }
+
+        return console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "no". \n Let's try again, ${name}!`);
+    };
+
+    gameCalcBeginning(0);
 };
