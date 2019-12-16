3841acb36f8e6a7221d7b961ec666a137c58c294
Evgeny <bespalove.mir@gmail.com>
2019-12-16 22:57:38 +0300

    Переписывает шаблон brain-even

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,31 +1,17 @@
-import readlineSync from 'readline-sync';
-import { randomNumber, getUserName } from '../index';
+import { randomNumber, pair, startGame } from '../index';
 
+// Создадим описание игры
+const description = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+// Создадим логику игры
 const gameEven = () => {
-    console.log('Answer "yes" if the number is even, otherwise answer "no". \n');
-    const name = getUserName();
-    console.log('\n');
+    const question = randomNumber(1, 20);
+    const correctAnswer = question % 2 === 0 ? 'yes' : 'no';
 
-    const gameEvenBeginning = (counter = 0) => {
-        const questionNumber = randomNumber(1, 20);
-        console.log(`Question: ${questionNumber}`);
-
-        const userAnswer = readlineSync.question('Your answer: ');
-        const correctAnswer = questionNumber % 2 === 0 ? 'yes' : 'no';
-
-        if (correctAnswer === userAnswer) {
-            if (counter === 2) {
-                return console.log(`Congratulations, ${name}!`);
-            }
-
-            console.log('Correct!');
-            return gameEvenBeginning(counter + 1);
-        }
-
-        return console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was '${correctAnswer}'. \n Let's try again, ${name}!`);
-    };
-
-    gameEvenBeginning(0);
+    return pair(question, correctAnswer);
 };
 
-export default gameEven;
+// Запишем в результат вызов игры из index.js с передачей данных
+const result = () => startGame(description, gameEven);
+
+export default result;
