dd5705c4e54d8fb22a2554f2acc8eb2115dc0bea
Evgeny <bespalove.mir@gmail.com>
2019-12-22 21:27:14 +0300

    Исправляет магическое число 4 в brain-calc

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -5,20 +5,21 @@ export const description = 'What is the result of the expression?';
 export const gameCalc = () => {
     const numberOne = randomNumber(1, 5);
     const numberTwo = randomNumber(1, 5);
-    const index = randomNumber(1, 4);
+    const operations = ['+', '-', '*'];
+    const indexOperation = randomNumber(1, operations.length - 1);
     let question = '';
     let correctAnswer = '';
 
-    switch (index) {
-    case 1:
+    switch (indexOperation) {
+    case 0:
         question = `${numberOne} + ${numberTwo}`;
         correctAnswer = String(numberOne + numberTwo);
         break;
-    case 2:
+    case 1:
         question = `${numberOne} - ${numberTwo}`;
         correctAnswer = String(numberOne - numberTwo);
         break;
-    case 3:
+    case 2:
         question = `${numberOne} * ${numberTwo}`;
         correctAnswer = String(numberOne * numberTwo);
         break;
